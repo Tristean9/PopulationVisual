1d1860024c007c2101d5347b7d8d6a7d24fa5434
Tristean9 <1367927606@qq.com>
2024-05-26 22:27:25 +0800

    增加了封面和引言，配置了网页部署命令

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+	<cover />
+	<introduction />
+</template>
+
+<script setup>
+import Cover from "./components/Cover.vue";
+import Introduction from "./components/Introduction.vue";
+</script>
+
+<style scoped></style>
---
+++ src/components/Introduction.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="container">
+		<img :src="imagePath" />
+	</div>
+</template>
+
+<script setup>
+const imagePath = `${import.meta.env.BASE_URL}introduction.png`;
+
+console.log(imagePath);
+</script>
+
+<style scoped>
+.container {
+	display: inline-block;
+	position: relative; /* 如果需要定位图片 */
+}
+
+.container img {
+	width: 100%; /* 宽度充满容器 */
+	height: auto; /* 高度自适应，保持图片原有宽高比 */
+	/* min-height: 800px; */
+}
+</style>
